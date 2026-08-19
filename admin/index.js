7f024a020e98d4fbf44f580e52902586dbc76544
mrzetrixyt-525 <mrzetrixyt@gmail.com>
2026-08-19 09:53:10 +0600

    Free Status Page

--- admin/index.js
+++ admin/index.js
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1">
-<title id="pageTitle">MrZetrix Status Admin</title>
+<title id="pageTitle">Status Admin</title>
 <script src="https://cdn.tailwindcss.com"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 <style>
